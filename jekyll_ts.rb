464852d7bdb57ed6ef2ebadfed02d1d59942a5c0
Patrick Lavigne <me@patrick.codes>
2016-07-13 13:35:56 -0400

    Add Configuration and Error Handling
    - Add tsc_opts, tsc_dev_opts and tsc_prod_opts configuration parameters
      to allow passing params to tsc
    - Add environment check to allow production vs development builds
    - Switch use of `` process execution to use of Open3 execution to
      capture output
    - Change logging output to use Jekyll.logger everywhere
    - Add log debug statements for easier debugging
    - Add check for compilation failure and error logging
    - Wrap command paths in quotes to help avoid spaces breaking command
    - Update readme with description of new configuration

--- jekyll_ts.rb
+++ jekyll_ts.rb
@@ -1,11 +1,22 @@
 # Converts typescript files to javascript
 #
 # Uses the following configs
-#    tsc => path of typescript compiler,
-#    js_dest => output of js files after compilation
+#    tsc           => Path of typescript compiler
+#    js_dest       => Output of js files after compilation, relative to site root (usually "_site")
+#    environment   => Build environment, either "production" (default) or "development".
+#                     Overridden by environment variable "JEKYLL_ENV"
+#    tsc_opts      => Options applied to every build regardless of environment. Defaults to "-t ES5" for
+#                     backwards-compatability
+#    tsc_dev_opts  => Options added to builds when environment is "development". No default
+#    tsc_prod_opts => Options added to builds when environment is "production". No default
 
 # Written by Matt Sheehan, 2015
+# Updated by Patrick Lavigne, 2016
 
+require 'open3'
+
+# Friendly name to show in logs
+PLUGIN_NAME = "Compile Typescript:"
 
 module Jekyll
 
@@ -14,12 +25,32 @@ module Jekyll
 		priority :low
 
 		def generate(site)
+            Jekyll.logger.debug(PLUGIN_NAME, "Initializing")
 			# location of typescript compiler
 			# defaults to tsc assuming in system env path
 			tsc = site.config["tsc"] || "tsc"
 
 			# js destination
 			js_dest = site.config["js_dest"] || "/"
+
+
+            # Environment can be optionally set in an environment variable or site config. Default to "production" to
+            # maintain old behavior if not found
+            jekyll_env = ENV["JEKYLL_ENV"] || site.config["environment"] || "production"
+
+            # Extra options to always pass to tsc
+            opts = site.config["tsc_opts"] || "-t ES5"
+            case jekyll_env
+                # Extra options to pass to tsc only for prod builds
+            when "production"
+                opts += " " + site.config["tsc_prod_opts"]
+
+                # Extra options to pass to tsc only for dev builds
+            when "development"
+                opts += " " + site.config["tsc_dev_opts"]
+            end
+
+            Jekyll.logger.debug(PLUGIN_NAME, "Will compile typescript in #{jekyll_env} mode with options: #{opts}")
 
 			ts_files = Array.new;
 
@@ -31,9 +62,9 @@ module Jekyll
 				ts_name = File.basename(sf.path)
 
 				# add ts file
-				ts_files << TsFile.new(site, site.source, ts_dir, ts_name, js_dest, tsc)
-				puts sf.path
-				# return true so this file gets removed from static_files
+				ts_files << TsFile.new(site, site.source, ts_dir, ts_name, js_dest, tsc, opts, jekyll_env)
+
+                # return true so this file gets removed from static_files
 				# we'll replace it with our own tsfile that implements
 				# it's own write
 				true
@@ -46,12 +77,14 @@ module Jekyll
 
 
 	class TsFile < StaticFile
-		def initialize(site, base, dir, name, jsroot, tsc)
+		def initialize(site, base, dir, name, jsroot, tsc, opts, env)
 			super(site, base, dir, name, nil)
 
 			@tspath = File.join(base, dir, name)
 			@jsdir = jsroot
 			@tsc = tsc
+            @opts = opts;
+            @env = env;
 		end
 
 		def write(dest)
@@ -65,12 +98,27 @@ module Jekyll
 
 			# make sure dir exists
 			FileUtils.mkdir_p(js_path)
-			puts js
 			# execute shell command
 			begin
-				command = "#{@tsc} -t ES5 --out #{js} #{@tspath}"
+				command = "#{@tsc} #{@opts} --out \"#{js}\" \"#{@tspath}\" >&1"
 
-				`#{command}`
+				stdout, result = Open3.capture2(command)
+
+                unless result.success?
+                    lines = stdout.split("\n")
+                    Jekyll.logger.error(PLUGIN_NAME, "Error: #{lines[0]}")
+                    first = true
+                    lines.each do |v|
+                        if first
+                            first = false
+                            next
+                        end
+                        Jekyll.logger.error(PLUGIN_NAME, "  #{v}");
+                    end
+                    Jekyll.logger.abort_with(PLUGIN_NAME, "Compilation failed.")
+                end
+
+                Jekyll.logger.debug(PLUGIN_NAME, "Compiled #{@tspath} to #{js}")
 			end
 		end
 	end
--- readme.md
+++ readme.md
@@ -3,8 +3,52 @@ Jekyll plugin to compile Typescript
 
 ## Setup
 
-1. Add *jekyll_ts.rb* to _plugins folder
-2. Add plugin configs to your _config.yml file
-    * tsc:     path of typescript compiler
-    * js_dest: output of js files after compilation
-3. Javascript files will write to js_dest location on build
+1. Add *jekyll_ts.rb* to `_plugins` folder
+2. Add plugin configs to your `_config.yml` file (see [Configuration Options](#configuration-options), below)
+3. Javascript files will write to `js_dest` location on build
+
+## Configuration Options
+
+The following options can be specified in your `_config.yml`:
+
+#### `tsc`
+*Default: `tsc`*
+
+Command to use as the typescript compiler. The default is usually enough if you can run "tsc" on the command line already.
+
+#### `js_dest`
+*Default: `/`*
+
+Location to write compiled .js files to. Relative to your site output directory (usually `_site`)
+
+#### `environment`
+*Default: `production`*
+
+Build environment to use. Per Jekyll's specs, this can be set to `production` or `development`. If unset, we default to
+assuming it is `production` for backwards-compatibility. 
+
+You can also specify this value in the environment variable `JEKYLL_ENV`, which will override the value in the config.
+
+#### `tsc_opts`
+*Default: `-t ES5`*
+
+Arguments to be passed to the `tsc` command in addition to the file to process and the `--out` destination. Arguments
+here will always be used regardless of the `environment`.
+
+#### `tsc_dev_opts`
+*Default: unset*
+
+Arguments to be passed to the `tsc` command only when the `environment` is set to `development`. They will be appended
+to any arguments in `tsc_opts`
+
+#### `tsc_prod_opts`
+*Default: unset*
+
+Arguments to be passed to the `tsc` command only when the `environment` is set to `production`. They will be appended
+to any arguments in `tsc_opts`
+
+## Original Author
+[Matt Sheehan](https://github.com/sheehamj13)
+
+## Contributors
+* [Patrick Lavigne](https://github.com/PMLavigne)
